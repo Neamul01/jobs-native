e9e16c4ded49b855f04d5454e083fffdf97b017b
Neamul01 <neamulhaque2002@gmail.com>
2023-10-26 14:15:29 +0600

    fb auth added

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,17 +1,68 @@
-import { Text, StyleSheet, View } from "react-native";
-import React, { Component } from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Facebook from "expo-auth-session/providers/facebook"; // Use * to import all from Facebook
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default class AuthScreen extends Component {
-  render() {
-    return (
-      <SafeAreaView>
-        <View>
-          <Text>AuthScreen</Text>
-        </View>
-      </SafeAreaView>
-    );
-  }
+const AuthScreen = () => {
+  const [user, setUser] = useState();
+  const [request, response, promptAsync] = Facebook.useAuthRequest({
+    clientId: "321370610537862",
+  });
+
+  useEffect(() => {
+    if (response && response.type === "success" && response.authentication) {
+      const fetchUserInfo = async () => {
+        // Removed the async IIFE
+        const userInfoResponse = await fetch(
+          `https://graph.facebook.com/me?access_token=${response.authentication.accessToken}&fields=id,name,picture.type(large)`
+        );
+        const userInfo = await userInfoResponse.json();
+        setUser(userInfo);
+      };
+
+      fetchUserInfo(); // Call the fetchUserInfo function
+    }
+  }, [response]);
+
+  const handlePressAsync = async () => {
+    const result = await promptAsync();
+    if (result.type !== "success") {
+      alert("Something went wrong");
+      return;
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>AuthScreen</Text>
+        {user ? (
+          <Profile user={user} /> // Pass the user data to the Profile component
+        ) : (
+          <Button
+            disabled={!request}
+            title="Sign in with Facebook"
+            onPress={handlePressAsync}
+          />
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+function Profile({ user }) {
+  return (
+    <View>
+      <Image
+        source={{ uri: user.picture.data.url }}
+        style={{ height: 200, width: 200 }}
+      />
+      <Text>{user.name}</Text>
+      <Text>ID: {user.id}</Text>
+    </View>
+  );
 }
+
+export default AuthScreen;
 
 const styles = StyleSheet.create({});
